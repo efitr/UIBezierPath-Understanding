3c7d07db8330aa892eb85018ce6261cfaaba1cd8
Egon Fiedler <efiedler93@gmail.com>
2018-12-21 22:30:06 -0500

    sample file for starting

---
+++ UIBezierPath/DemoView.swift
@@ -0,0 +1,21 @@
+//
+//  DemoView.swift
+//  UIBezierPath
+//
+//  Created by Egon Fiedler on 12/21/18.
+//  Copyright © 2018 App Solutions. All rights reserved.
+//
+
+import UIKit
+
+class DemoView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
