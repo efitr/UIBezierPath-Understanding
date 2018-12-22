6c577d2771ce291f2d414d652bb950cb20a78fca
Egon Fiedler <efiedler93@gmail.com>
2018-12-21 21:21:58 -0500

    Begginig the project, currently no added code

---
+++ UIBezierPath/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIBezierPath
+//
+//  Created by Egon Fiedler on 12/21/18.
+//  Copyright © 2018 App Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
