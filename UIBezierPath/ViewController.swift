f9b4880fe06cbb8d032579a4e937a75abf256602
Egon Fiedler <efiedler93@gmail.com>
2018-12-22 01:22:42 -0500

    This is the next state after previous work making a grey rectangle

--- UIBezierPath/ViewController.swift
+++ UIBezierPath/ViewController.swift
@@ -13,8 +13,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    // This function will appear if it's true
+    override func viewDidAppear(_ animated: Bool) {
+        //What exactly is this doing?
+        super.viewDidAppear(animated)
+        
+        let width: CGFloat = 240.0
+        let height: CGFloat = 160.0
+        
+        let demoView = DemoView(frame: CGRect(x: self.view.frame.size.width/2 - width/2,
+                                              y: self.view.frame.size.height/2 - height/2,
+                                              width: width,
+                                              height: height))
+        //
+        self.view.addSubview(demoView)
+    }
     
 
 }
