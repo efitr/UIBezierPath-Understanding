6f6986a42bfe02a31b91438b6faa17fb87703c14
Egon Fiedler <efiedler93@gmail.com>
2018-12-22 01:24:45 -0500

    added rectangule function

--- UIBezierPath/DemoView.swift
+++ UIBezierPath/DemoView.swift
@@ -30,5 +30,24 @@ class DemoView: UIView {
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+    
+    func createRectangule() {
+        path = UIBezierPath()
+        
+        // Specify the point that the path should start get drawn.
+        path.move(to: CGPoint(x: 0.0, y: 0.0))
+        
+        // Create a line between the starting point and the bottom-left side of the view.
+        path.addLine(to: CGPoint(x: 0.0, y: self.frame.size.height))
+        
+        // Create the bottom line (bottom-left to bottom-right).
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: self.frame.size.height))
+        
+        // Create the vertical line from the bottom-right to the top-right side.
+        path.addLine(to: CGPoint(x: self.frame.size.width, y: 0.0))
+        
+        // Close the path. This will create the last line automatically.
+        path.close()
+    }
 
 }
