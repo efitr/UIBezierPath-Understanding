38a076955074600055e760c8b39cbbc98eb2e4b8
Egon Fiedler <efiedler93@gmail.com>
2018-12-25 22:09:59 -0500

    mastering a tutorial, yet the actual thing

--- UIBezierPath/DemoView.swift
+++ UIBezierPath/DemoView.swift
@@ -13,51 +13,78 @@ class DemoView: UIView {
     
     var path: UIBezierPath!
     
-
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-        //self.createRectangle()
-        //self.createTriangle()
-        
-//        self.path = UIBezierPath(ovalIn: self.bounds)
-        
-//        self.path = UIBezierPath(ovalIn: CGRect(x: self.frame.size.width/2 - self.frame.size.height/2,
-//                                                y: 0.0,
-//                                                width: self.frame.size.height,
-//                                                height: self.frame.size.height))
-        
-//        path = UIBezierPath(roundedRect: self.bounds, cornerRadius: 15.0)
-        
-//        path = UIBezierPath(roundedRect: self.bounds,
-//                            byRoundingCorners: [.topLeft, .bottomRight],
-//                            cornerRadii: CGSize(width: 15.0, height: 0.0))
+// Comes with a price in performance
+//    override func draw(_ rect: CGRect) {
+//        // Drawing code
+//        //self.createRectangle()
+//        //self.createTriangle()
 //
-
+////        self.path = UIBezierPath(ovalIn: self.bounds)
+//
+////        self.path = UIBezierPath(ovalIn: CGRect(x: self.frame.size.width/2 - self.frame.size.height/2,
+////                                                y: 0.0,
+////                                                width: self.frame.size.height,
+////                                                height: self.frame.size.height))
+//
+////        path = UIBezierPath(roundedRect: self.bounds, cornerRadius: 15.0)
+//
+////        path = UIBezierPath(roundedRect: self.bounds,
+////                            byRoundingCorners: [.topLeft, .bottomRight],
+////                            cornerRadii: CGSize(width: 15.0, height: 0.0))
+////
+//
+////        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2),
+////                            radius: self.frame.size.height/2,
+////                            startAngle: CGFloat(80.0).toRadians(),
+////                            endAngle: CGFloat(180.0).toRadians(),
+////                            clockwise: true)
+//
 //        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2),
 //                            radius: self.frame.size.height/2,
-//                            startAngle: CGFloat(80.0),
-//                            endAngle: CGFloat(180.0),
-//                            clockwise: true)
+//                            startAngle: CGFloat(240.0).toRadians(),
+//                            endAngle: CGFloat(15.0).toRadians(),
+//                            clockwise: false)
+//
+//        // Specify the fill color and apply it to the path.
+//        UIColor.orange.setFill()
+//        path.fill()
+//
+//        // Specify a border (stroke) color.
+//        UIColor.purple.setStroke()
+//        path.stroke()
+//    }
+    
+    
+    func maskVsSublayer() {
+        self.createTriangle()
         
-        path = UIBezierPath(arcCenter: CGPoint(x: self.frame.size.width/2, y: self.frame.size.height/2),
-                            radius: self.frame.size.height/2,
-                            startAngle: CGFloat(240.0),
-                            endAngle: CGFloat(15.0),
-                            clockwise: false)
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.fillColor = UIColor.yellow.cgColor
         
-        // Specify the fill color and apply it to the path.
-        UIColor.orange.setFill()
-        path.fill()
-        
-        // Specify a border (stroke) color.
-        UIColor.purple.setStroke()
-        path.stroke()
+        self.layer.addSublayer(shapeLayer)
     }
 
+    
+    func simpleShapeLayer() {
+        self.createRectangle()
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = self.path.cgPath
+        
+        shapeLayer.fillColor = UIColor.yellow.cgColor
+        shapeLayer.strokeColor = UIColor.brown.cgColor
+        shapeLayer.lineWidth = 3.0
+        
+        self.layer.addSublayer(shapeLayer)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         self.backgroundColor = UIColor.darkGray
+        
+        maskVsSublayer()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -91,4 +118,10 @@ class DemoView: UIView {
         path.close()
     }
 
+}
+
+extension CGFloat {
+    func toRadians() -> CGFloat {
+        return self * CGFloat(Double.pi) / 180.0
+    }
 }
